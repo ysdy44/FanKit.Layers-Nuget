4e46efc4471dd4cd48092803d6f20858d3b238fd
ysdy44 <ysdy44@outlook.com>
2025-02-04 04:40:38 +0800

    TestApp Add DependencyObjectExtensions

---
+++ FanKit.Layers.TestApp/DependencyObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace FanKit.Layers.TestApp
+{
+    public static class DependencyObjectExtensions
+    {
+        public static Point PointToWindow(this UIElement visual) => visual.TransformToVisual(Window.Current.Content).TransformPoint(default);
+        public static ScrollViewer FindDescendantScrollViewer(this ListView listView) => listView.FindDescendantScrollViewerCore();
+        private static ScrollViewer FindDescendantScrollViewerCore(this DependencyObject parent)
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is ScrollViewer scrollViewer)
+                    return scrollViewer;
+
+                if (child.FindDescendantScrollViewerCore() is ScrollViewer scrollViewerChild)
+                    return scrollViewerChild;
+            }
+
+            return null;
+        }
+    }
+}
