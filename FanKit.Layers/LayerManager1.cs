9f46cb3631f0f96e2e4d276065347970bbb4a303
ysdy44 <ysdy44@outlook.com>
2025-02-03 16:29:36 +0800

    File Add LayerManager1

---
+++ FanKit.Layers/LayerManager1.cs
@@ -0,0 +1,23 @@
+﻿using FanKit.Layers.Collections;
+using System;
+
+namespace FanKit.Layers
+{
+    /// <include file="doc/docs.xml" path="docs/doc[@for='LayerManager1']/*" />
+    public class LayerManager1<T>
+        where T : class, ILayerBase, IDisposable
+    {
+        private readonly LogicalTreeList<T> LogicalTree;
+
+        /// <include file="doc/docs.xml" path="docs/doc[@for='LayerManager1.List']/*" />
+        public readonly LayerList<T> List;
+
+        /// <include file="doc/docs.xml" path="docs/doc[@for='LayerManager1.LayerManager1']/*" />
+        public LayerManager1()
+        {
+            this.LogicalTree = new LogicalTreeList<T>();
+
+            this.List = new LayerList<T>(this.LogicalTree);
+        }
+    }
+}
