2689a223561eca40bfd0414cd8ef7b297b027f7b
ysdy44 <ysdy44@outlook.com>
2025-02-13 23:19:19 +0800

    Sample.UWP Add Strings

--- FanKit.Layers.Sample.Uwp/Strings/LanguageCommand.cs
+++ FanKit.Layers.Sample.Uwp/Strings/LanguageCommand.cs
@@ -6,14 +6,65 @@ using Windows.UI.Xaml.Controls;
 
 namespace FanKit.Layers.Sample
 {
-    public class LanguageCommand : ICommand
+    public class LanguageCommand : CultureInfoCollection, ICommand
     {
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter) => true;
-
         public async void Execute(object parameter)
         {
+            string language = $"{parameter}";
+
+            if (string.IsNullOrEmpty(language))
+            {
+                SetLanguageEmpty();
+
+                await new MessageDialog(
+                    UIType.RestartApp.GetString(),
+                    UIType.UseSystemSetting.GetString()
+                    ).ShowAsync();
+            }
+            else
+            {
+                SetLanguage(language);
+
+                await new MessageDialog(
+                    UIType.RestartApp.GetString(),
+                    new CultureInfo(language).NativeName
+                    ).ShowAsync();
+            }
+
+            await Windows.ApplicationModel.Core.CoreApplication.RequestRestartAsync(string.Empty);
+        }
+
+        public MenuBarItem ToMenuBarItem()
+        {
+            MenuBarItem bar = new MenuBarItem
+            {
+                Title = UIType.Language.GetString(),
+                Items =
+                {
+                    new MenuFlyoutItem
+                    {
+                        Text = UIType.UseSystemSetting.GetString(),
+                        CommandParameter = string.Empty,
+                        Command = this
+                    },
+                    new MenuFlyoutSeparator(),
+                }
+            };
+
+            foreach (CultureInfo item in this)
+            {
+                bar.Items.Add(new MenuFlyoutItem
+                {
+                    Text = item.NativeName,
+                    CommandParameter = item.Name,
+                    Command = this
+                });
+            }
+
+            return bar;
         }
     }
 }
