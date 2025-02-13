171ad4736988b70ed1b5eccee3e3980c39fbcd7b
ysdy44 <ysdy44@outlook.com>
2025-02-13 22:46:17 +0800

    Sample.Shared Add History

---
+++ FanKit.Layers.Sample.Shared/History/ChangeGroup.cs
@@ -0,0 +1,31 @@
+﻿using FanKit.Layers.Changes;
+using System;
+using System.Collections.Generic;
+
+namespace FanKit.Layers.Sample
+{
+    public class ChangeGroup : Dictionary<HistoryType, IChange>, IChange
+    {
+        public IEnumerable<Guid> ReferenceGuids
+        {
+            get
+            {
+                foreach (KeyValuePair<HistoryType, IChange> child in this)
+                {
+                    foreach (Guid item in child.Value.ReferenceGuids)
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (KeyValuePair<HistoryType, IChange> child in this)
+            {
+                child.Value.Dispose();
+            }
+        }
+    }
+}
---
+++ FanKit.Layers.Sample.Shared/History/RectChanges.cs
@@ -0,0 +1,27 @@
+﻿using FanKit.Layers.Changes;
+using System;
+using System.Collections.Generic;
+
+namespace FanKit.Layers.Sample
+{
+    public sealed class RectChanges : IChange
+    {
+        public RectChange[] Rects { get; set; }
+
+        public IEnumerable<Guid> ReferenceGuids
+        {
+            get
+            {
+                foreach (RectChange item in this.Rects)
+                {
+                    yield return item.Id;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Rects = null;
+        }
+    }
+}
