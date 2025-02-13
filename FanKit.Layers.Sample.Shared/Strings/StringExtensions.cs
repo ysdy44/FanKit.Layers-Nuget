4540511a68d2777726f0d113454611701bad172b
ysdy44 <ysdy44@outlook.com>
2025-02-13 22:47:50 +0800

    Sample.Shared Add Strings

---
+++ FanKit.Layers.Sample.Shared/Strings/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace FanKit.Layers.Sample
+{
+    public static class StringExtensions
+    {
+        public static string GetString(this OptionCatalog catalog)
+        {
+            return App.Resource.GetString($"{catalog}");
+        }
+
+        public static string GetString(this OptionType type)
+        {
+            return App.Resource.GetString($"{type}");
+        }
+
+        public static string GetString(this UIType type)
+        {
+            return App.Resource.GetString($"{type}");
+        }
+    }
+}
