aac8df40a72c9a8d46598c061a31a60997123500
ysdy44 <ysdy44@outlook.com>
2025-02-16 19:19:10 +0800

    Sample.WPF Add Keyboards

---
+++ FanKit.Layers.Sample.Wpf/Keyboards/KeyboardAccelerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows.Input;
+
+namespace FanKit.Layers.Sample
+{
+    public readonly struct KeyboardAccelerator
+    {
+        public readonly Key Key;
+        public readonly ModifierKeys Modifiers;
+
+        public KeyboardAccelerator(string keyboardAccelerator)
+        {
+            string[] split = keyboardAccelerator.Split('+');
+
+            this.Key = (Key)System.Enum.Parse(typeof(Key), split.Last());
+            switch (split.First())
+            {
+                case "Ctrl":
+                    this.Modifiers = ModifierKeys.Control;
+                    break;
+                case "Shift":
+                    this.Modifiers = ModifierKeys.Shift;
+                    break;
+                default:
+                    this.Modifiers = ModifierKeys.None;
+                    break;
+            }
+        }
+    }
+}
