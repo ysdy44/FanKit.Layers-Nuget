6de3aa549f7e6ea3fa47584c2bba38dc292be84c
ysdy44 <ysdy44@outlook.com>
2025-02-17 00:24:21 +0800

    Sample.WinUI3 Add Canvas

---
+++ FanKit.Layers.Sample.WinUI3/Canvas/Thumbnail.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.IO;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Graphics.Effects;
+
+namespace FanKit.Layers.Sample
+{
+    public class Thumbnail : IDisposable
+    {
+        const int W = 50;
+        const int H = 50;
+        const int Dpi = 96;
+
+        public readonly WriteableBitmap WriteableBitmap;
+        public readonly CanvasRenderTarget RenderTarget;
+        public readonly ImageBrush ImageBrush;
+
+        public Thumbnail(ICanvasResourceCreator resourceCreator)
+        {
+            this.WriteableBitmap = new WriteableBitmap(W, H);
+            this.RenderTarget = new CanvasRenderTarget(resourceCreator, W, H, Dpi);
+
+            this.ImageBrush = new ImageBrush
+            {
+                ImageSource = this.WriteableBitmap
+            };
+        }
+
+        private Thumbnail(Thumbnail thumbnail)
+        {
+            this.WriteableBitmap = new WriteableBitmap(W, H);
+            thumbnail.WriteableBitmap.PixelBuffer.CopyTo(this.WriteableBitmap.PixelBuffer);
+
+            CanvasDevice device = thumbnail.RenderTarget.Device;
+            this.RenderTarget = new CanvasRenderTarget(device, W, H, Dpi);
+            this.RenderTarget.SetPixelBytes(thumbnail.RenderTarget.GetPixelBytes());
+
+            this.ImageBrush = new ImageBrush
+            {
+                ImageSource = this.WriteableBitmap
+            };
+        }
+
+        public Thumbnail Clone() => new Thumbnail(this);
+
+        public void Invalidate(IGraphicsEffectSource bitmap, float w, float h, Rect rect)
+        {
+            using (CanvasDrawingSession ds = this.RenderTarget.CreateDrawingSession())
+            {
+                if (rect.Height < 0)
+                {
+                    if (rect.Width < 0)
+                    {
+                        if (rect.Width < rect.Height)
+                        {
+                            float sx = (float)(-50 / w);
+                            float sy = (float)(-50 / rect.Width * rect.Height / h);
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.Clear(Colors.Black);
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                        else
+                        {
+                            float sx = (float)(-50 / rect.Height * rect.Width / w);
+                            float sy = (float)(-50 / h);
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.Clear(Colors.Black);
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                    }
+                    else
+                    {
+                        if (rect.Width > -rect.Height)
+                        {
+                            float sx = 50 / w;
+                            float sy = (float)(50 / rect.Width * rect.Height / h);
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.Clear(Colors.Black);
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                        else
+                        {
+                            float sx = (float)(-50 / rect.Height * rect.Width / w);
+                            float sy = -50 / h;
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.Clear(Colors.Black);
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    if (rect.Width < 0)
+                    {
+                        if (-rect.Width > rect.Height)
+                        {
+                            float sx = -50 / w;
+                            float sy = (float)(-50 / rect.Width * rect.Height / h);
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                        else
+                        {
+                            float sx = (float)(50 / rect.Height * rect.Width / w);
+                            float sy = 50 / h;
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                    }
+                    else
+                    {
+                        if (rect.Width < rect.Height)
+                        {
+                            float sx = (float)(50 / rect.Height * rect.Width / w);
+                            float sy = 50 / h;
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                        else
+                        {
+                            float sx = 50 / w;
+                            float sy = (float)(50 / rect.Width * rect.Height / h);
+
+                            Matrix3x2 m = Matrix3x2.CreateTranslation(-w / 2, -h / 2)
+                                * Matrix3x2.CreateScale(sx, sy)
+                                * Matrix3x2.CreateTranslation(25, 25);
+
+                            ds.DrawImage(new Transform2DEffect
+                            {
+                                BorderMode = EffectBorderMode.Hard,
+                                InterpolationMode = CanvasImageInterpolation.NearestNeighbor,
+                                Source = bitmap,
+                                TransformMatrix = m
+                            });
+                        }
+                    }
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        public void Invalidate()
+        {
+            byte[] bytes = this.RenderTarget.GetPixelBytes();
+            using (Stream stream = this.WriteableBitmap.PixelBuffer.AsStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+
+            this.WriteableBitmap.Invalidate();
+        }
+
+        public void Dispose() => this.RenderTarget.Dispose();
+    }
+}
---
+++ FanKit.Layers.Sample.WinUI3/Canvas/Transformer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.UI;
+using Windows.Foundation;
+
+namespace FanKit.Layers.Sample
+{
+    public struct Transformer
+    {
+        public Rect Rect;
+
+        public float Left12;
+        public float Top12;
+        public float Right12;
+        public float Bottom12;
+
+        public float Left8;
+        public float Top8;
+        public float Right8;
+        public float Bottom8;
+
+        public void Draw(CanvasDrawingSession drawingSession)
+        {
+            if (this.Rect.IsEmpty)
+                return;
+
+            drawingSession.DrawRectangle(this.Rect, Colors.DodgerBlue, 2f);
+
+            drawingSession.FillRectangle(this.Left12, this.Top12, 12, 12, Colors.DodgerBlue);
+            drawingSession.FillRectangle(this.Right12, this.Top12, 12, 12, Colors.DodgerBlue);
+            drawingSession.FillRectangle(this.Left12, this.Bottom12, 12, 12, Colors.DodgerBlue);
+            drawingSession.FillRectangle(this.Right12, this.Bottom12, 12, 12, Colors.DodgerBlue);
+
+            drawingSession.FillRectangle(this.Left8, this.Top8, 8f, 8f, Colors.White);
+            drawingSession.FillRectangle(this.Right8, this.Top8, 8f, 8f, Colors.White);
+            drawingSession.FillRectangle(this.Left8, this.Bottom8, 8f, 8f, Colors.White);
+            drawingSession.FillRectangle(this.Right8, this.Bottom8, 8f, 8f, Colors.White);
+        }
+
+        public static implicit operator Transformer(Rect rect)
+        {
+            return new Transformer
+            {
+                Rect = rect,
+
+                Left12 = (float)(rect.X - 6f),
+                Top12 = (float)(rect.Y - 6f),
+                Right12 = (float)(rect.X + rect.Width - 6f),
+                Bottom12 = (float)(rect.Y + rect.Height - 6f),
+
+                Left8 = (float)(rect.X - 4f),
+                Top8 = (float)(rect.Y - 4f),
+                Right8 = (float)(rect.X + rect.Width - 4f),
+                Bottom8 = (float)(rect.Y + rect.Height - 4f)
+            };
+        }
+    }
+}
