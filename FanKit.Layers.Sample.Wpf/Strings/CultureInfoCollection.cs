79ff77337a840e4c3672686d0fa4bcf9d5729e72
ysdy44 <ysdy44@outlook.com>
2025-02-16 16:42:37 +0800

    Sample.WPF Add Strings

---
+++ FanKit.Layers.Sample.Wpf/Strings/CultureInfoCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+
+namespace FanKit.Layers.Sample
+{
+    public class CultureInfoCollection : List<CultureInfo>
+    {
+        //@Static
+        public static bool IsRightToLeft => CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft;
+        public static FlowDirection FlowDirection => IsRightToLeft ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+
+        public CultureInfoCollection() : base(
+            from a
+            in ApplicationLanguages.ManifestLanguages
+            select new CultureInfo(a))
+        {
+        }
+
+        public static void SetLanguageEmpty()
+        {
+            if (ApplicationLanguages.PrimaryLanguageOverride == string.Empty) return;
+            ApplicationLanguages.PrimaryLanguageOverride = string.Empty;
+        }
+
+        public static void SetLanguage(string language)
+        {
+            if (ApplicationLanguages.PrimaryLanguageOverride == language) return;
+            ApplicationLanguages.PrimaryLanguageOverride = language;
+        }
+    }
+}
--- FanKit.Layers.Sample.Wpf/Strings/LanguageCommand.cs
+++ FanKit.Layers.Sample.Wpf/Strings/LanguageCommand.cs
@@ -6,14 +6,66 @@ using System.Windows.Input;
 
 namespace FanKit.Layers.Sample
 {
-    public class LanguageCommand : ICommand
+    public class LanguageCommand : CultureInfoCollection, ICommand
     {
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter) => true;
-
         public void Execute(object parameter)
         {
+            string language = $"{parameter}";
+
+            if (string.IsNullOrEmpty(language))
+            {
+                SetLanguageEmpty();
+
+                MessageBox.Show(
+                    UIType.RestartApp.GetString(),
+                    UIType.UseSystemSetting.GetString()
+                    );
+            }
+            else
+            {
+                SetLanguage(language);
+
+                MessageBox.Show(
+                    UIType.RestartApp.GetString(),
+                    new CultureInfo(language).NativeName
+                    );
+            }
+
+            System.Diagnostics.Process.Start(ApplicationLanguages.Execute);
+            System.Environment.Exit(0);
+        }
+
+        public MenuItem ToMenuBarItem()
+        {
+            MenuItem bar = new MenuItem
+            {
+                Header = UIType.Language.GetString(),
+                Items =
+                {
+                    new MenuItem
+                    {
+                        Header = UIType.UseSystemSetting.GetString(),
+                        CommandParameter = string.Empty,
+                        Command = this
+                    },
+                    new Separator(),
+                }
+            };
+
+            foreach (CultureInfo item in this)
+            {
+                bar.Items.Add(new MenuItem
+                {
+                    Header = item.NativeName,
+                    CommandParameter = item.Name,
+                    Command = this
+                });
+            }
+
+            return bar;
         }
     }
 }
