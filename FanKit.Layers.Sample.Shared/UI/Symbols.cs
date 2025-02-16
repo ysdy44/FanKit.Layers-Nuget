5e56a4f4886c226657ae9f7981c33007692dc02d
ysdy44 <ysdy44@outlook.com>
2025-02-16 15:40:56 +0800

    Sample.Shared Add Symbols

---
+++ FanKit.Layers.Sample.Shared/UI/Symbols.cs
@@ -0,0 +1,38 @@
+﻿namespace FanKit.Layers.Sample
+{
+    // FontFamily: Segoe Fluent Icons Regular
+    // File: SegoeIcons.ttf
+    public enum Symbols
+    {
+        Save = 57605, // E105
+        Clear = 57606, // E106
+        Delete = 57607, // E107
+        Redo = 57613, // E10D
+        Undo = 57614, // E10E
+        Mail = 57625, // E119
+        TwoPage = 57630, // E11E
+        ClosePane = 57639, // E127
+        Page = 57650, // E132
+        ViewAll = 57656, // E138
+        Refresh = 57673, // E149
+        SelectAll = 57678, // E14E
+        Import = 57680, // E150
+        ImportAll = 57681, // E151
+        Read = 57702, // E166
+        Link = 57703, // E167
+        ShowBcc = 57705, // E169
+        HideBcc = 57706, // E16A
+        Cut = 57707, // E16B
+        Paste = 57709, // E16D
+        Copy = 57711, // E16F
+        Important = 57713, // E171
+        NewWindow = 57724, // E17C
+        Folder = 57736, // E188
+        View = 57739, // E18B
+        OpenFile = 57765, // E1A5
+        BackToWindow = 57816, // E1D8
+        FullScreen = 57817, // E1D9
+        NewFolder = 57818, // E1DA
+        Preview = 58005, // E295
+    }
+}
