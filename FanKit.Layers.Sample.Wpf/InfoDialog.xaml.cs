3ac4084283376d422ba16e61656276dec6e99e1a
ysdy44 <ysdy44@outlook.com>
2025-02-16 16:54:20 +0800

    Sample.WPF Add InfoDialog

---
+++ FanKit.Layers.Sample.Wpf/InfoDialog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace FanKit.Layers.Sample
+{
+    public sealed partial class InfoDialog : Window
+    {
+        public InfoDialog(ILayer layer)
+        {
+            this.InitializeComponent();
+            base.Title = layer.Title.ToString();
+            this.CloseButton.Content = UIType.UIBack.GetString();
+            this.CloseButton.Click += delegate { base.Close(); };
+
+            this.ListView.ItemsSource = layer.Children;
+            this.HeaderBorder.Child = new TextBlock
+            {
+                Inlines =
+                {
+                    new Run { Text = UIType.InfoDepth.GetString() },
+                    new Run { Text = layer.Depth.ToString() },
+                    new LineBreak(),
+
+                    new Run { Text = UIType.InfoIsExpanded.GetString() },
+                    new Run { Text = layer.IsExpanded.ToString() },
+                    new LineBreak(),
+
+                    new Run { Text = UIType.InfoIsLocked.GetString() },
+                    new Run { Text = layer.IsLocked.ToString() },
+                    new LineBreak(),
+
+                    new Run { Text = UIType.InfoIsVisible.GetString() },
+                    new Run { Text = layer.IsVisible.ToString() },
+                    new LineBreak(),
+
+                    new Run { Text = UIType.InfoSelectMode.GetString() },
+                    new Run { Text = layer.SelectMode.ToString() },
+                    new LineBreak(),
+
+                    new Run { Text = UIType.InfoChildren.GetString() },
+                    new LineBreak(),
+                }
+            };
+        }
+    }
+}
