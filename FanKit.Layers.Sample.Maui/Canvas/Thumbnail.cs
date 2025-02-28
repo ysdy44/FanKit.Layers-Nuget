66423da37f6bc3ed7b46291db5bd14414cf46dd5
ysdy44 <ysdy44@outlook.com>
2025-03-01 00:00:48 +0800

    Sample.Maui Add Canvas

---
+++ FanKit.Layers.Sample.Maui/Canvas/Thumbnail.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace FanKit.Layers.Sample
+{
+    public sealed class Thumbnail
+    {
+        public Brush ImageBrush;
+        public ImagePaint ImagePaint = new ImagePaint();
+
+        public Thumbnail(Thumbnail thumbnail)
+        {
+            ImagePaint = new ImagePaint
+            {
+                Image = thumbnail.ImagePaint.Image
+            };
+            ImageBrush = ImagePaint;
+        }
+
+        public Thumbnail(GraphicsView canvasControl)
+        {
+            ImagePaint = new ImagePaint();
+            ImageBrush = ImagePaint;
+        }
+
+        public void Invalidate(IImage bitmap, float bitmapWidth, float bitmapHeight, Rect rect)
+        {
+            ImagePaint = new ImagePaint
+            {
+                Image = bitmap
+            };
+            ImageBrush = ImagePaint;
+        }
+
+        public Thumbnail Clone()
+        {
+            return new Thumbnail(this);
+        }
+    }
+}
---
+++ FanKit.Layers.Sample.Maui/Canvas/Transformer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace FanKit.Layers.Sample
+{
+    public struct Transformer
+    {
+        public Rect Rect;
+
+        public float Left12;
+        public float Top12;
+        public float Right12;
+        public float Bottom12;
+
+        public float Left8;
+        public float Top8;
+        public float Right8;
+        public float Bottom8;
+
+        public void Draw(ICanvas canvas)
+        {
+            if (this.Rect.IsEmpty)
+                return;
+
+            canvas.StrokeSize = 2f;
+            canvas.StrokeColor = Colors.DodgerBlue;
+            canvas.DrawRectangle(this.Rect);
+
+            canvas.FillColor = Colors.DodgerBlue;
+            canvas.FillRectangle(this.Left12, this.Top12, 12, 12);
+            canvas.FillRectangle(this.Right12, this.Top12, 12, 12);
+            canvas.FillRectangle(this.Left12, this.Bottom12, 12, 12);
+            canvas.FillRectangle(this.Right12, this.Bottom12, 12, 12);
+
+            canvas.FillColor = Colors.White;
+            canvas.FillRectangle(this.Left8, this.Top8, 8f, 8f);
+            canvas.FillRectangle(this.Right8, this.Top8, 8f, 8f);
+            canvas.FillRectangle(this.Left8, this.Bottom8, 8f, 8f);
+            canvas.FillRectangle(this.Right8, this.Bottom8, 8f, 8f);
+        }
+
+        public static implicit operator Transformer(Rect rect)
+        {
+            return new Transformer
+            {
+                Rect = rect,
+
+                Left12 = (float)(rect.X - 6f),
+                Top12 = (float)(rect.Y - 6f),
+                Right12 = (float)(rect.X + rect.Width - 6f),
+                Bottom12 = (float)(rect.Y + rect.Height - 6f),
+
+                Left8 = (float)(rect.X - 4f),
+                Top8 = (float)(rect.Y - 4f),
+                Right8 = (float)(rect.X + rect.Width - 4f),
+                Bottom8 = (float)(rect.Y + rect.Height - 4f)
+            };
+        }
+    }
+}
