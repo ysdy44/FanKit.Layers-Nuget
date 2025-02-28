1d34cb043803b77caf197030ea3a8c665c46f1c2
ysdy44 <ysdy44@outlook.com>
2025-03-01 00:10:01 +0800

    Sample.Maui Add Menus

---
+++ FanKit.Layers.Sample.Maui/Menus/CatalogToolbarItem.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FanKit.Layers.Sample
+{
+    public class CatalogToolbarItem : ToolbarItem
+    {
+        public readonly Dictionary<string, OptionType> Dictionary = new Dictionary<string, OptionType>();
+
+        public readonly string[] Buttons;
+
+        public MainPage XamlRoot;
+
+        public CatalogToolbarItem(OptionCatalogMenu catalog)
+        {
+            foreach (OptionTypeMenu item in catalog.Items)
+            {
+                if (item is null)
+                    continue;
+
+                string key = item.Type.GetString();
+
+                if (this.Dictionary.ContainsKey(key))
+                    continue;
+
+                this.Dictionary.Add(key, item.Type);
+            }
+
+            this.Buttons = this.Dictionary.Keys.ToArray();
+
+            base.Text = catalog.Catalog.GetString();
+        }
+
+        protected override async void OnClicked()
+        {
+            base.OnClicked();
+
+            string result = await this.XamlRoot.DisplayActionSheet(
+                base.Text,
+                UIType.UIBack.GetString(),
+                UIType.UIBack.GetString(),
+                this.Buttons);
+
+            if (this.Dictionary.ContainsKey(result))
+            {
+                this.XamlRoot.Command.Execute(this.Dictionary[result]);
+            }
+        }
+    }
+}
---
+++ FanKit.Layers.Sample.Maui/Menus/LanguageToolbarItem.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FanKit.Layers.Sample
+{
+    public class LanguageToolbarItem : ToolbarItem
+    {
+        public readonly Dictionary<string, string> Dictionary = new Dictionary<string, string>();
+
+        public readonly string[] Buttons;
+
+        public MainPage XamlRoot;
+
+        public LanguageToolbarItem(IEnumerable<CultureInfo> cultures)
+        {
+            this.Dictionary.Add(UIType.UseSystemSetting.GetString(), string.Empty);
+
+            foreach (CultureInfo item in cultures)
+            {
+                this.Dictionary.Add(item.NativeName, item.Name);
+            }
+
+            this.Buttons = this.Dictionary.Keys.ToArray();
+
+            base.Text = UIType.Language.GetString();
+        }
+
+        protected override async void OnClicked()
+        {
+            base.OnClicked();
+
+            string result = await this.XamlRoot.DisplayActionSheet(
+                UIType.Language.GetString(),
+                UIType.UIBack.GetString(),
+                UIType.UIBack.GetString(),
+                this.Buttons);
+
+            if (this.Dictionary.ContainsKey(result))
+            {
+                this.XamlRoot.LanguageCommand.Execute(this.Dictionary[result]);
+            }
+        }
+    }
+}
