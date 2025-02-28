38f2cbd62d74c3644ed9400cb72925ffc95663ad
ysdy44 <ysdy44@outlook.com>
2025-03-01 00:10:50 +0800

    Sample.Maui Add Reorders

---
+++ FanKit.Layers.Sample.Maui/Reorders/ReorderScrollView.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace FanKit.Layers.Sample
+{
+    public class ReorderScrollView : ScrollView
+    {
+        readonly DropGestureRecognizer Reorder = new DropGestureRecognizer
+        {
+            AllowDrop = true
+        };
+
+        public event EventHandler<DragEventArgs> DragOver
+        {
+            remove => this.Reorder.DragOver -= value;
+            add => this.Reorder.DragOver += value;
+        }
+
+        public event EventHandler<DragEventArgs> DragLeave
+        {
+            remove => this.Reorder.DragLeave -= value;
+            add => this.Reorder.DragLeave += value;
+        }
+
+        public event EventHandler<DropEventArgs> Drop
+        {
+            remove => this.Reorder.Drop -= value;
+            add => this.Reorder.Drop += value;
+        }
+
+        public bool AllowDrop
+        {
+            get => base.GestureRecognizers.Count > 0;
+            set
+            {
+                base.GestureRecognizers.Remove(this.Reorder);
+                if (value)
+                    base.GestureRecognizers.Add(this.Reorder);
+            }
+        }
+    }
+}
